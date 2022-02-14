1dce4fdcf830e2f2c1e8ff9f330f01f7af1a6277
VrindaTyagi123 <83289238+VrindaTyagi123@users.noreply.github.com>
2022-02-14 07:33:01 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -12,10 +12,10 @@ Webcam.attach( '#camera' );
 function take_snapshot()
 {
     Webcam.snap(function(data_uri){
-        document.getElementById("result".innerHTML = '<img id="captured_image" src="'+data_uri+'"/>');
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
     });
 }
 
 console.log('ml5 version:', ml5.version);
 
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/sPbjEYdBu/', modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/v_sl95BzE/model.json',modelLoaded);
